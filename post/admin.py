46ca7c9e404fd6ff586c6e8203c2f715337935bd
Awos <uws0331@gmail.com>
2021-07-02 21:34:07 +0900

    post앱의 관리자 홈페이지 생성

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from post.models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['author', 'content']
+    list_display = ('author', 'content')
+
+
+admin.site.register(Post, PostAdmin)
