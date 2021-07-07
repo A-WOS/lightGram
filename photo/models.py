9daabdf1b52ac8f504a997fdb026f7405c5b660f
Awos <uws0331@gmail.com>
2021-07-07 21:02:42 +0900

    photo 모델 설계

--- photo/models.py
+++ photo/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    text = models.TextField(blank=True)
+    image = models.ImageField(upload_to='timeline_photo/%Y/%m/%d')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "text : " + self.text
+
+    class Meta:
+        ordering = ['-created']
