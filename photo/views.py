a0911452dbc78ce872e5d9f404d0690288f66890
Awos <uws0331@gmail.com>
2021-07-07 21:20:33 +0900

    PhotoList, Create, Update, Delete, Detail을 class 형 뷰의 generic view를 이용하여 구현

--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from photo.models import Photo
 
-# Create your views here.
+
+#
+class PhotoList(ListView):
+    model = Photo
+    template_name_suffix = '_list'
+
+
+class PhotoCreate(CreateView):
+    model = Photo
+    fields = ['text', 'image']
+    template_name_suffix = '_create'
+    success_url = '/'
+
+
+class PhotoUpdate(UpdateView):
+    model = Photo
+    fields = ['text', 'image']
+    template_name_suffix = '_update'
+    success_url = '/'
+
+
+class PhotoDelete(DeleteView):
+    model = Photo
+    template_name_suffix = '_delete'
+    success_url = '/'
+
+
+class PhotoDetail(DetailView):
+    model = Photo
+    template_name_suffix = '_detail'
+
