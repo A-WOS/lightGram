1fbedb6f48fa8254dbfc9fbba0f2090b8c3f6f48
Awos <uws0331@gmail.com>
2021-07-07 21:02:18 +0900

    installed apps에 photo 추가

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 추가한 앱
-    # 'post.apps.PostConfig',
+    'photo.apps.PhotoConfig',
 ]
 
 MIDDLEWARE = [
