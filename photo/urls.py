599fc7e4b04ca2f3f2ecc495fb62a22fee940724
Awos <uws0331@gmail.com>
2021-07-08 17:42:58 +0900

    앞서 View에서 작성했던 Create, Update, Delete, List, Detail 뷰들을 연결시켜주기 위한 url

---
+++ photo/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+import photo
+from photo.views import PhotoCreate, PhotoDelete, PhotoUpdate, PhotoDetail, PhotoList
+
+app_name = 'photo'
+
+urlpatterns = [
+    path('create/', PhotoCreate.as_view(), name='create'),
+    path('delete/<int:pk>/', PhotoDelete.as_view(), name='delete'),
+    path('update/<int:pk>/', PhotoUpdate.as_view(), name='update'),
+    path('detail/<int:pk>/', PhotoDetail.as_view(), name='detail'),
+    path('', PhotoList.as_view(), name='index'),
+]
