09f0e713ef18a2a1948a2f837c470c996f29fbb7
Awos <uws0331@gmail.com>
2021-07-11 18:13:41 +0900

    삭제나 수정 주소로 접속해도 수정, 삭제가 안되도록 해결 작성자와 요청자가 다르면 수정할 권한이 없다는 메시지를 주고 메인페이지로 이동

--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from photo.models import Photo
@@ -28,13 +30,29 @@ class PhotoUpdate(UpdateView):
     model = Photo
     fields = ['text', 'image']
     template_name_suffix = '_update'
-    success_url = '/'
+    # success_url = '/'
+
+    def dispatch(self, request, *args, **kwargs):
+        object = self.get_object()
+        if object.author != request.user:
+            messages.warning(request, '수정할 권한이 없습니다.')
+            return HttpResponseRedirect('/')
+        else:
+            return super(PhotoUpdate, self).dispatch(request, *args, **kwargs)
 
 
 class PhotoDelete(DeleteView):
     model = Photo
     template_name_suffix = '_delete'
-    success_url = '/'
+    # success_url = '/'
+
+    def dispatch(self, request, *args, **kwargs):
+        object = self.get_object()
+        if object.author != request.user:
+            messages.warning(request, '삭제할 권한이 없습니다.')
+            return HttpResponseRedirect('/')
+        else:
+            return super(PhotoDelete, self).dispatch(request, *args, **kwargs)
 
 
 class PhotoDetail(DetailView):
