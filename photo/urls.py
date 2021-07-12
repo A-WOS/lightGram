c7b057018be25e29cfb9df6d83abd59ce5028f8e
Awos <uws0331@gmail.com>
2021-07-12 18:41:48 +0900

    like, favorite 구현하기

--- photo/urls.py
+++ photo/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.urls import path
 
 import photo
-from photo.views import PhotoCreate, PhotoDelete, PhotoUpdate, PhotoDetail, PhotoList
+from photo.views import PhotoCreate, PhotoDelete, PhotoUpdate, PhotoDetail, PhotoList, PhotoLike, PhotoFavorite
 
 app_name = 'photo'
 
@@ -13,6 +13,8 @@ urlpatterns = [
     path('update/<int:pk>/', PhotoUpdate.as_view(), name='update'),
     path('detail/<int:pk>/', PhotoDetail.as_view(), name='detail'),
     path('', PhotoList.as_view(), name='index'),
+    path('like/<int:photo_id>/', PhotoLike.as_view(), name='like'),
+    path('favorite/<int:photo_id>/', PhotoFavorite.as_view(), name='favorite'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
