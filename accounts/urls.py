3b3de64c95083ca005c442c52ead1b54e4626adf
Awos <uws0331@gmail.com>
2021-07-18 16:35:30 +0900

    회원가입 urls.py 작성

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
+from accounts.views import singup
+
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    path('signup/', singup, name='signup'),
 ]
