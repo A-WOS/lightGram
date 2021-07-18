b04d2797f6cc67e95a4088a48859a71e57e393e5
Awos <uws0331@gmail.com>
2021-07-18 16:35:20 +0900

    회원가입 views.py 작성

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
-# Create your views here.
+def singup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        print(username, password, password2)
+
+        # 회원 객체 생성하기
+        user = User()
+        user.username = username
+        user.password = password
+        user.set_password(password)
+        user.save()
+        return render(request, 'accounts/signup_complete.html')
+    else:
+        context_values = {'form':'this is form'}
+        return render(request, 'accounts/signup.html', context_values)
