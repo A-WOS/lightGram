ec5e61c50a2ce92348fc9dc2e9ea3e7389a3bc46
Awos <uws0331@gmail.com>
2021-07-19 17:41:03 +0900

    singup -> signup으로 변경

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accounts.views import singup
+from accounts.views import signup
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
-    path('signup/', singup, name='signup'),
+    path('signup/', signup, name='signup'),
 ]
