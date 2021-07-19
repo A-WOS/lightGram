fdc305b7526bd4d6d67dde9dac47b061619d29cf
Awos <uws0331@gmail.com>
2021-07-19 17:42:06 +0900

    중복 저장을 피할 수 있도록 views.py 수정

--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,37 @@
-from django.contrib.auth.models import User
-from django.shortcuts import render
+# from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
-def singup(request):
+from accounts.forms import SignUpForm
+
+
+def signup(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        password2 = request.POST.get('password2')
-        print(username, password, password2)
+        signup_form = SignUpForm(request.POST)
 
-        # 회원 객체 생성하기
-        user = User()
-        user.username = username
-        user.password = password
-        user.set_password(password)
-        user.save()
-        return render(request, 'accounts/signup_complete.html')
-    else:
-        context_values = {'form':'this is form'}
-        return render(request, 'accounts/signup.html', context_values)
+        if signup_form.is_valid():
+            user_instance = signup_form.save(commit=False)
+            user_instance.set_password(signup_form.cleaned_data['password'])
+            user_instance.save()
+            return redirect(request, 'accounts/signup_complete.html', {'username': user_instance.username})
+
+        else:
+            signup_form = SignUpForm()
+
+        return render(request, 'accounts/signup.html', {'form': signup_form.as_p})
+
+
+    #     username = request.POST.get('username')
+    #     password = request.POST.get('password')
+    #     password2 = request.POST.get('password2')
+    #     print(username, password, password2)
+    #
+    #     # 회원 객체 생성하기
+    #     user = User()
+    #     user.username = username
+    #     user.password = password
+    #     user.set_password(password)
+    #     user.save()
+    #     return render(request, 'accounts/signup_complete.html')
+    # else:
+    #     context_values = {'form':'this is form'}
+    #     return render(request, 'accounts/signup.html', context_values)
